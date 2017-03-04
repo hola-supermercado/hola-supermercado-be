a15aed7aeda44c404d98aa72afc2be41faea0ac7
Teoh <benjamin.teoh@capgemini.com>
2017-03-04 23:24:59 +0100

    allow Access-Control-Allow-Origin

--- src/server/app.js
+++ src/server/app.js
@@ -15,11 +15,23 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+})
+
 // Send a response to the index
 app.get('/', helloWorld.helloWorld);
 
 app.use('/beacons', beacons);
-
 // Start server on the specified port and binding host
 app.listen(port, () => {
   console.log(`Node Version: ${process.version}`);
